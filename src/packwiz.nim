d8218a8691745a2f195de95779401a6fb04bf91d
ExoPlant <lennyj2468+github@gmail.com>
2022-10-07 21:13:13 +0100

    update

--- src/packwiz.nim
+++ src/packwiz.nim
@@ -13,47 +13,45 @@
  See the License for the specific language governing permissions and
  limitations under the License. 
 ]#
-import logging # prolly most important stuff
-import nigui
 import std/os
 import std/osproc
+import strutils
+
+import ./logging
+import ./types
+
 let logger: Logger = getLogger "packwiz-nigui".createLoggingSource "packwiz"
 
 proc `^&`(packName: string): string =
-    return "instances" / packName # ahhhh ty!
+    return "instances" / packName
 
-# `/` is a built-in Nim function for the separator, so it works better and is cleaner
-proc checkIfPackwiz*(): bool = # also have a nice little warning there. 
-    var bool1: bool = "packwiz".findExe != "" or "packwiz.exe".findExe != ""
+
+proc checkIfPackwiz*(): bool =
+    result = "packwiz".findExe != "" or "packwiz.exe".findExe != ""
     if "packwiz.exe".fileExists or "packwiz".fileExists:
         logger.warn "Packwiz in current directory takes priority over packwiz in PATH. Rename or remove the packwiz in current directory to use the packwiz located in PATH"
-    return bool1 
 
-proc getPackwiz*(): string =
-    var pw: string
+proc getPackwiz*(): string = 
     when defined windows:
-        pw = "packwiz.exe"
+        result = "packwiz.exe"
     else:
-        pw = "packwiz"
-    var packwiz: string = ""
+        result = "packwiz"
     if not checkIfPackwiz():
         logger.error "No packwiz found. Please install packwiz into your PATH or the current directory. Note that packwiz-nigui will not work without this."
-    
-    if fileExists(pw):
-        packwiz = absolutePath pw 
-    else:
-        packwiz = pw
-    return packwiz
 
-proc packwizInit*(packName: string = "TestPack", packAuthor: string = "TestAuthor", packVersion: string = "1.0.0", mcVersion: string = "1.19.2", modloader: string = "vanilla", modloaderVersion: string = ""): ClickProc = 
+    if fileExists(result):
+        result = absolutePath result
+# loader should likely be an enum. modloaderversion would be easier than mcversion :P well, maybe not for forge
+# Lmao true, we'll keep it as strings for now
+proc packwizInit*(packName: string = "TestPack", packAuthor: string = "TestAuthor", packVersion: string = "1.0.0", mcVersion: string = "1.19.2", modloader: Modloader = Modloader.None, modloaderVersion: string = "") = 
     if not dirExists "instances":
         createDir "instances"
     if dirExists ^&packName:
         logger.error "Pack directory already exists! Delete " & ^&packName & " or choose a different name"
     else:
         createDir ^&packName
-        var args: seq[string] = @["init", "--name", packName, "--author", packAuthor, "--version", packVersion, "--mc-version", mcVersion, "--modloader", modloader]
-        if modloader != "vanilla":
-            args.add "--" & modloader & "-version"
-            args.add modloaderVersion
+        var args: seq[string] = @["init", "--name", packName, "--author", packAuthor, "--version", packVersion, "--mc-version", mcVersion, "--modloader", toLowerAscii($modloader)]
+        if $modloader != "none" and toLowerAscii($modloader) != "vanilla":
+            args.add "--" & toLowerAscii($modloader) & "-version" # so yeah, my code in types.nim is probably cursed, have you read it?
+            args.add modloaderVersion # w
         discard execProcess(getPackwiz(), ^&packName, args, options={poUsePath})
